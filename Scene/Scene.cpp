077b6b9626f343566feb78ae70fda1b8c94ec984
Rudraksha20 <rudraksha.shah@gmail.com>
2020-03-18 00:10:44 -0400

    stating some files that were left out

--- Scene/Scene.cpp
+++ Scene/Scene.cpp
@@ -88,9 +88,11 @@ void Scene::LoadScene(std::string fllePath) {
         }
 
         // Initialze the geometry
-        // Initialize geometry's model matrix
-        glm::mat4 modelMatrix = glm::mat4(1.0f);
-        std::shared_ptr<Geometry> geometry = std::make_shared<Geometry>(vertices, normals, uvs, triangleIndices, modelMatrix);
+        // Initialize geometry's transforms
+        glm::vec3 position = glm::vec3(0.0f, 1.0f, 0.0f);
+        glm::vec3 rotation = glm::vec3(0.0f, 45.0f, 0.0f);
+        glm::vec3 scale = glm::vec3(1.0f, 2.0f, 1.0f);
+        std::shared_ptr<Geometry> geometry = std::make_shared<Geometry>(vertices, normals, uvs, triangleIndices, position, rotation, scale);
         m_geometries.push_back(geometry);
 
         // TODO: Load Material
